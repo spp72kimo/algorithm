0db9ea626612e4d004ce9e5685bef99097ef8a7f
Steven Ke <spp72kimo@gmail.com>
2022-01-18 03:04:07 +0800

    Add insert_sort.py

---
+++ insert_sort.py
@@ -0,0 +1,15 @@
+import random
+
+x = random.sample(range(100),10)		# 隨機產生10個不重複的數字List
+print('')
+print('隨機產生10個數字清單：\n', x)
+
+for i in range(1,len(x)):				# x[i]是當前檢查數字的索引位置
+	temp = x[i]
+	j = i - 1							# x[j]是已經檢查好的索引位置
+	while temp < x[j] and j >= 0:		
+		x[j + 1] = x[j]
+		x[j] = temp
+		j -= 1
+print('')
+print('排序後的數字清單：\n', x)
